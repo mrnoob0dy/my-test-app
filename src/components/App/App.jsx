326df966f2b644b66459d3cd9a878e1aee05d76e
mrnoob0dy <jayna5155@yandex.ru>
2025-03-13 20:51:13 +0300

    First commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { MyName } from '../MyName/MyName'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello React!</h1>
+        <MyName></MyName>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,7 @@
+import './MyName.css';
+
+export function MyName() {
+	return <div>
+		<h3>My Name is Alex</h3>
+	</div>
+}
