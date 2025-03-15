3509216e5f03f22ce29f15f124248fd2a3babc5b
mrnoob0dy <jayna5155@yandex.ru>
2025-03-15 20:29:02 +0300

    Рендеринг списков в React

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,10 +11,28 @@ export function HomePage() {
     setClick(click + 1)
   }
 
+  const numbers = [1, 2, 3, 4, 5, 6, 7]
+  const listItems = numbers.map(num => <li key={num}>{num}</li>)
+  const users = [
+    {id: 1, name: 'Alice'},
+    {id: 2, name: 'Mike'},
+    {id: 3, name: 'Nick'},
+    {id: 4, name: 'Jane'},
+  ]
+
   return (
     <div>
       <h2>Домашняя страница</h2>
       <p>Добро пожаловать на наш сайт</p>
+      <ul>
+        {listItems}
+      </ul>
+      <ul>
+        {numbers.map(num => <li key={num}>{num}</li>)}
+      </ul>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
       <Button label="Click me" onClick={() => {handleClick()}} />
       <Button label={click} onClick={handleSetClick}/>
     </div>
