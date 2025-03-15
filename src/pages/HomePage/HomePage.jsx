aa17277ace3289989d0b6acf71e507cdf4478da4
mrnoob0dy <jayna5155@yandex.ru>
2025-03-15 19:46:19 +0300

    useState - управление состоянием в функциональных компонентах

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react"
 import { Button } from "../../components/Button/Button"
 
 export function HomePage() {
+  const [click, setClick] = useState(0)
   const handleClick = () => {
     alert('Меня нажали')
+  }
+
+  const handleSetClick = () => {
+    setClick(click + 1)
   }
 
   return (
     <div>
       <h2>Домашняя страница</h2>
       <p>Добро пожаловать на наш сайт</p>
-      <Button label="Click me" onClick={() => {
-        handleClick()
-      }} />
+      <Button label="Click me" onClick={() => {handleClick()}} />
+      <Button label={click} onClick={handleSetClick}/>
     </div>
   )
 }
